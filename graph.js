8c572bf90c3ec3483d4145200e053429324ade0f
Michael Rapadas <mike@mrap.me>
2014-04-17 16:06:30 -0700

    Find Hub by unique ID.

--- graph.js
+++ graph.js
@@ -2,15 +2,28 @@
 var Hub = require('./hub.js')
 
 var Graph = function Graph(){
-  this.hubs = new Array()
+  this._hubs = new Object()
 }
 
 Graph.prototype = {
+
   addHub: function addHub(hub){
-    if (hub instanceof Hub) this.hubs.push(hub)
+    if (hub instanceof Hub && !this.containsHub(hub))
+      this._hubs[hub.uid()] = hub
   },
+
   size: function size(){
-    return this.hubs.length
+    var count = 0
+    for(elem in this._hubs) count++
+    return count
+  },
+
+  containsHub: function containsHub(hub) {
+    return this.containsHubWithID(hub.uid())
+  },
+
+  containsHubWithID: function containsHubWithID(hubUID) {
+    return this._hubs.hasOwnProperty(hubUID)
   }
 
 }
--- hub.js
+++ hub.js
@@ -1,28 +1,40 @@
-  
 
 var EMPTY_VERTEX = -1
-var Hub = function Hub(verticiesCount, initialDistance){
-  this.adj = new Array()
-  for(var v = 0; v < verticiesCount; v++) this.adj[v] = initialDistance
+var Hub = function Hub(uid, verticiesCount, initialDistance){
+  this._adj = new Array()
+  this._uid = uid
+  for(var v = 0; v < verticiesCount; v++) this._adj[v] = initialDistance
 }
 
 Hub.prototype = {
+  uid: function() {
+    return this._uid
+  },
+
+  toString: function() {
+    return this._uid
+  },
+
+  addVertex: function addVertex(vertex, distance){
+    if (distance > 0) this._adj[vertex] = distance
+  },
+
   updateVertex: function updateVertex(vertex, distance){
-    this.adj[vertex] = distance
+    if (distance > 0) this._adj[vertex] = distance
   },
 
   deleteVertex: function deleteVertex(vertex) {
-    this.adj[vertex] = EMPTY_VERTEX
+    this._adj[vertex] = EMPTY_VERTEX
   },
 
   size: function size(){
-    return this.adj.length
+    return this._adj.length
   },
 
   contains: function contains(vertex){
     return (vertex >= 0 &&
             vertex < this.size() &&
-            this.adj[vertex] != EMPTY_VERTEX)
+            this._adj[vertex] != EMPTY_VERTEX)
   }
 }
 
