082596c1dcfc0eb976d0702b216582e2d16e3fe2
Michael Rapadas <mike@mrap.me>
2014-04-17 15:19:30 -0700

    Hub gets .contains() .deleteVertex()

--- hub.js
+++ hub.js
@@ -1,4 +1,6 @@
   
+
+var EMPTY_VERTEX = -1
 var Hub = function Hub(verticiesCount, initialDistance){
   this.adj = new Array()
   for(var v = 0; v < verticiesCount; v++) this.adj[v] = initialDistance
@@ -8,8 +10,19 @@ Hub.prototype = {
   updateVertex: function updateVertex(vertex, distance){
     this.adj[vertex] = distance
   },
+
+  deleteVertex: function deleteVertex(vertex) {
+    this.adj[vertex] = EMPTY_VERTEX
+  },
+
   size: function size(){
     return this.adj.length
+  },
+
+  contains: function contains(vertex){
+    return (vertex >= 0 &&
+            vertex < this.size() &&
+            this.adj[vertex] != EMPTY_VERTEX)
   }
 }
 
